4b04f4bcbbd60c20e0bac57be6b8d3a45fcfd166
Ahmed BenElHajHamza <benelhajhamza.ahmed@gmail.com>
2021-11-12 11:58:23 +0100

    reviews contrpller done

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -1,4 +1,6 @@
 class ListsController < ApplicationController
+  before_action :set_list, only: [:show, :destroy]
+
   def index
     @lists = List.all
   end
@@ -22,6 +24,10 @@ class ListsController < ApplicationController
   end
 
   private
+
+  def set_list
+    @list = List.find(params[:id])
+  end
 
   def list_params
     params.require(:list).permit(:name)
